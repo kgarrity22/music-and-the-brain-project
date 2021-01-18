7d2e4f9da9b266f96389a5f6a605a5ac72b23716
kgarrity22 <garrityk@bc.edu>
2021-01-18 09:29:18 -0500

    created new function to make creating filters simple

--- react-web-app-main/src/routes/dashboard/components/tabulator/index.js
+++ react-web-app-main/src/routes/dashboard/components/tabulator/index.js
@@ -7,55 +7,17 @@ import "react-tabulator/lib/styles.css"; // default theme
 import "react-tabulator/css/bootstrap/tabulator_bootstrap.min.css"; // use Theme(s)
 
 
-let data = [];
 
-function getTableData(){
-      var Airtable = require('airtable');
-      var base = new Airtable({apiKey: 'keygbNFWvzaP9t8xi'}).base('appmh47tLfNhe7i80');
-
-        var tab_ind = 0
-        var table_data = []
-
-      return new Promise((resolve, reject) => {
-        base('Trials').select({
-
-            // filterByFormula: airtableFilters,
-            view: "Raw View"
-        }).eachPage(function page(records, fetchNextPage) {
-
-            records.forEach(function(record) {
-
-              record.fields["id"] = tab_ind
-              tab_ind = tab_ind + 1;
-              table_data.push(record.fields)
-
-            });
-            fetchNextPage();
-        }, function done(err) {
-            if (err) {
-              console.error(err);
-              return reject({});
-            }
-
-            for (var record of table_data){
-              for (var key of Object.keys(record)){
-                if (typeof(record[key] !== "String")){
-                  record[key] = record[key].toString()
-                }
-              }
-            }
-            var tabledata = {}
-            tabledata["tabledata"] = table_data
-            resolve(tabledata)
-        })
-      })
-  }
 
   let options = {
     height: 500,
-    placeholder: "Loading Data...",
+    placeholder:"Loading Data...",
     downloadDataFormatter: (data) => data,
     downloadReady: (fileContents, blob) => blob,
+    resizable:false,
+    virtualDomBuffer:1000,
+    tooltips:true,
+
   };
 
   let columns = [
@@ -82,13 +44,6 @@ function getTableData(){
       ];
 
 
-    async function setData() {
-      const res = await getTableData()
-      console.log("RES: ", res.tabledata)
-      const new_data = res.tabledata
-      data = new_data
-    }
-    setData()
 
 
 class MainTable extends React.Component {
@@ -110,7 +65,7 @@ class MainTable extends React.Component {
         <ReactTabulator
           ref={ref => (this.ref = ref)}
           columns={columns}
-          data={data}
+          data={this.props.tabledata}
           options={options}
         />
       </div>
@@ -119,109 +74,3 @@ class MainTable extends React.Component {
 }
 
 export default MainTable;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from 'react'
-// import axios from 'axios'
-// import ReactTable from "react-table";
-// import 'react-table/react-table.css'
-//
-// export default class MainTable extends Component {
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       users: [],
-//       loading:true
-//     }
-//   }
-//
-//
-//     // const columns = [
-//     // { title: "Age Groups", field: "Age_Groups", width: 150 },
-//     // { title: "Conditions", field: "Conditions", hozAlign: "left", width: 150 },
-//     // { title: "Countries", field: "Geography_Countries", hozAlign: "left", width: 150  },
-//     // { title: "Enrollment", field: "Enrollment", hozAlign: "left", width: 150 },
-//     // { title: "Enrollment Target", field: "Enrollment_Target", hozAlign: "left", width: 150 },
-//     // { title: "Settings", field: "Facility_Settings", hozAlign: "left", width: 150 },
-//     // { title: "Regions", field: "Geography_Regions", hozAlign: "left", width: 150 },
-//     // { title: "Intervention Types", field: "Intervention_Types", hozAlign: "left", width: 150 },
-//     // { title: "Interventions", field: "Interventions", hozAlign: "left", width: 150 },
-//     // { title: "NCT", field: "NCT", hozAlign: "left", width: 150 },
-//     // { title: "Outcomes", field: "Outcome_Concepts", hozAlign: "left", width: 150 },
-//     // { title: "Phase", field: "Phase", hozAlign: "left", width: 150 },
-//     // { title: "Purpose", field: "Purpose", hozAlign: "left", width: 150 },
-//     // { title: "Randomization", field: "Randomization", hozAlign: "left", width: 150 },
-//     // { title: "Single/Multi Site", field: "Single_Multi_Site", hozAlign: "left", width: 150 },
-//     // { title: "Sponsor", field: "Sponsor", hozAlign: "left", width: 150 },
-//     // { title: "Start Year", field: "Start_Year", hozAlign: "left", width: 150 },
-//     // { title: "Status", field: "Status", hozAlign: "left", width: 150 },
-//     // { title: "Study Type", field: "Study_Type", hozAlign: "left", width: 150 },
-//     // { title: "Title", field: "Title", hozAlign: "left", width: 150 }
-//     // ];
-//
-//
-//
-//   async getUsersData(){
-//
-//     const res = await getTableData()
-//     console.log(res.tabledata)
-//     this.setState({loading:false, users: res.tabledata})
-//   }
-//   componentDidMount(){
-//     this.getUsersData()
-//   }
-//   render() {
-//     const columns = [{
-//       Header: "Age Groups",
-//       accessor: "Age_Groups",
-//      }
-//      ,{
-//       Header: "Conditions",
-//       accessor: "Conditions",
-//
-//       }
-//
-//      ,{
-//      Header: "Countries",
-//      accessor: "Geography_Countries",
-//      }
-//      ,{
-//      Header: "Enrollment",
-//      accessor: "Enrollment",
-//
-//      },
-//      {
-//       Header: "Settings",
-//       accessor: "Facility_Settings",
-//
-//       },
-//       {
-//         Header: "Regions",
-//         accessor: "Geography_Regions",
-//
-//         }
-//   ]
-//     return (
-//       <ReactTable
-//       data={this.state.users}
-//       columns={columns}
-//    />
-//     )
-//   }
-// }
