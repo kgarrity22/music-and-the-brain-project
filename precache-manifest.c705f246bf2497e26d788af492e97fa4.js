be595fb817fb6f2d8dd3e727474928ae442e0e85
kgarrity22 <garrityk@bc.edu>
2021-01-30 17:05:54 -0500

    Updates

---
+++ precache-manifest.c705f246bf2497e26d788af492e97fa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61519279fdefc49449d67eeb797eb7ea",
+    "url": "/Prism-Generic-Canvas/index.html"
+  },
+  {
+    "revision": "875a5979cc9355749080",
+    "url": "/Prism-Generic-Canvas/static/css/2.880185b9.chunk.css"
+  },
+  {
+    "revision": "f88328d3d39ea6835b60",
+    "url": "/Prism-Generic-Canvas/static/css/main.777ed86a.chunk.css"
+  },
+  {
+    "revision": "875a5979cc9355749080",
+    "url": "/Prism-Generic-Canvas/static/js/2.7c1c3da1.chunk.js"
+  },
+  {
+    "revision": "07be89ec6ad29e7ac93c829801c29fd7",
+    "url": "/Prism-Generic-Canvas/static/js/2.7c1c3da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88328d3d39ea6835b60",
+    "url": "/Prism-Generic-Canvas/static/js/main.ebf945d2.chunk.js"
+  },
+  {
+    "revision": "441b07ab7db77c990843",
+    "url": "/Prism-Generic-Canvas/static/js/runtime-main.3dfdaea3.js"
+  }
+]);
