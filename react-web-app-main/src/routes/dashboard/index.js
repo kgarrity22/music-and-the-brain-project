ff1861c26c0ca9ec1815b09375e79577acd62a43
heyspencer <heyspencer@gmail.com>
2021-03-12 15:50:35 -0500

    Added an arrrow

--- react-web-app-main/src/routes/dashboard/index.js
+++ react-web-app-main/src/routes/dashboard/index.js
@@ -3211,7 +3211,7 @@ function createSunburst(level1, level2, level3, data) {
                   </Col>
                   <Col lg={{span: 6}}>
                     <PrismTextBlock
-                      textTitle={ 'How did we classify interventions?' }
+                      textTitle={ '<- How did we classify interventions?' }
                       mainText={ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
                     />
                   </Col>
