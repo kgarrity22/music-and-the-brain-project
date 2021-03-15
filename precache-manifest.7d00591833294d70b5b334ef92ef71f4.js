f7aa74b687707882dfafcb8515ae92e29974d90b
kgarrity22 <garrityk@bc.edu>
2021-03-15 13:10:33 -0400

    Updates

--- precache-manifest.7d00591833294d70b5b334ef92ef71f4.js
+++ precache-manifest.7d00591833294d70b5b334ef92ef71f4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "affe0b29613ee2c888447c1772b429b3",
+    "revision": "03426301fc3c47d54b7ac9caba426479",
     "url": "/music-and-the-brain-project/index.html"
   },
   {
-    "revision": "cbe4c81bacf2ddf4be65",
+    "revision": "c2554e05c137bd0793f0",
     "url": "/music-and-the-brain-project/static/css/2.880185b9.chunk.css"
   },
   {
-    "revision": "08c621d58196eec99b52",
+    "revision": "5a0f12b5f44ab14c4e82",
     "url": "/music-and-the-brain-project/static/css/main.0339bdda.chunk.css"
   },
   {
-    "revision": "cbe4c81bacf2ddf4be65",
-    "url": "/music-and-the-brain-project/static/js/2.240af391.chunk.js"
+    "revision": "c2554e05c137bd0793f0",
+    "url": "/music-and-the-brain-project/static/js/2.6ab6f10f.chunk.js"
   },
   {
     "revision": "004b0376fc0091600e6750a21f5d3488",
-    "url": "/music-and-the-brain-project/static/js/2.240af391.chunk.js.LICENSE.txt"
+    "url": "/music-and-the-brain-project/static/js/2.6ab6f10f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "08c621d58196eec99b52",
-    "url": "/music-and-the-brain-project/static/js/main.d666d27f.chunk.js"
+    "revision": "5a0f12b5f44ab14c4e82",
+    "url": "/music-and-the-brain-project/static/js/main.3a3fea07.chunk.js"
   },
   {
     "revision": "63fa09f7401da4777619",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music-and-the-brain-project/precache-manifest.5819b004da3c6b70fbc9da67d8219811.js"
+  "/music-and-the-brain-project/precache-manifest.7d00591833294d70b5b334ef92ef71f4.js"
 );
 
 self.addEventListener('message', (event) => {
