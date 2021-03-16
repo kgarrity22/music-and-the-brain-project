67d672a47aceb787a6f91773d7ab773009fd26e7
kgarrity22 <garrityk@bc.edu>
2021-03-15 20:10:19 -0400

    adjusted height of modal

--- react-web-app-main/src/routes/dashboard/index.js
+++ react-web-app-main/src/routes/dashboard/index.js
@@ -3185,6 +3185,16 @@ function createSunburst(level1, level2, level3, data) {
                       loading={loadingTrialsData}
                     />
                   </Col>
+                  <Col lg={{span: 6}}>
+
+                    <PrismTextBlock
+                      paddingTop={90}
+                      paddingBottom={0}
+                      arrow={<FaArrowCircleLeft/ >}
+                      textTitle={ '     Lorem Ipsum?' }
+                      mainText={ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+                    />
+                  </Col>
                   </Row>
                   <Row>
                   <Col lg={{span: 6}}>
